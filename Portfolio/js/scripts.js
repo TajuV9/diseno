ab490484dab93969ef3fb6681942c9e9ce1e52b0
Cristian <131365312+TajuV9@users.noreply.github.com>
2025-02-07 17:48:44 +0100

    Create scripts.js

---
+++ Portfolio/js/scripts.js
@@ -0,0 +1,9 @@
+// Leaflet Map
+
+const map = L.map('map').setView([40.416775, -3.703790], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
